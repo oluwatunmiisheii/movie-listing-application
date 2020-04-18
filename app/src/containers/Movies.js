e2f969a46809a98535c90a0795be959f2407e38f
Oluwatunmise <oluwatunmiseadenuga@crenettechlabs.com>
2020-04-18 02:01:10 +0100

    Feat: delete a single movie

---
+++ app/src/containers/Movies.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react'
+import { getMovies } from '../services/fakeMovieService'
+
+class Movies extends Component {
+  state = { movies: getMovies() }
+
+  deleteMovieHandler = movie => {
+    let { movies: stateMovies } = { ...this.state }
+    const updateMovies = stateMovies.filter(m => {
+      return m._id !== movie._id
+    })
+    this.setState({ movies: updateMovies })
+  }
+  render() {
+    const { movies } = this.state
+    let content
+
+    if (movies.length === 0) {
+      content = <p>There are no movies in the database</p>
+    } else {
+      content = (
+        <React.Fragment>
+          <p>Showing {movies.length} in the database</p>
+          <table className="table">
+            <thead>
+              <tr>
+                <th>Title</th>
+                <th>Genre</th>
+                <th>Stock</th>
+                <th>Rate</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              {movies.map(movie => (
+                <tr key={movie._id}>
+                  <td>{movie.title}</td>
+                  <td>{movie.genre.name}</td>
+                  <td>{movie.numberInStock}</td>
+                  <td>{movie.dailyRentalRate}</td>
+                  <td>
+                    <button
+                      onClick={() => this.deleteMovieHandler(movie)}
+                      className="btn btn-danger btn-sm"
+                    >
+                      <i className="fa fa-trash-o pr-1"></i> Delete
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </React.Fragment>
+      )
+    }
+    return (
+      <div className="row">
+        <div className="col-12 mt-5 pt-5">{content}</div>
+      </div>
+    )
+  }
+}
+
+export default Movies
--- app/src/index.js
+++ app/src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.css';
-import 'font-awesome/css/font-awesome-css'
+import 'font-awesome/css/font-awesome.css';
 ReactDOM.render(
   <React.StrictMode>
     <App />
